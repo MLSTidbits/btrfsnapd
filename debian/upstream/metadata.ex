054182f26c0335a98395e009b5cc36830dc9cccd
Michael Schaecher <MichaelLeeSchaecher@gmail.com>
2025-07-05 13:53:01 -0600

    Add initial Debian packaging files for btrfsnapd

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/btrfsnapd/issues
+# Bug-Submit: https://github.com/<user>/btrfsnapd/issues/new
+# Changelog: https://github.com/<user>/btrfsnapd/blob/master/CHANGES
+# Documentation: https://github.com/<user>/btrfsnapd/wiki
+# Repository-Browse: https://github.com/<user>/btrfsnapd
+# Repository: https://github.com/<user>/btrfsnapd.git
